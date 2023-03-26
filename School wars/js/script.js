671b6d03e99ff32c60557728bc16a17a9f8e58a3
casafresca <54688129+casafresca@users.noreply.github.com>
2023-03-25 23:41:58 -0400

    added the triangle and diamond (diamond still need some work), added a count down and added a new font

--- School wars/js/script.js
+++ School wars/js/script.js
@@ -12,12 +12,16 @@ author, and this description to match your project!
 "use strict";
 var mode;
 var shapeOptions = [/*Square, Circle, Triangle, Diamond, blue, red, green, yellow*/]; 
+var timer = 25;
+var speedUp = 100;
+var chalkFont;
+
 
 /**
 Description of preload
 */
 function preload() {
-
+    chalkFont = loadFont('assets/fonts/Chalktastic-r78L.ttf');
 }
 
 /**
@@ -41,15 +45,12 @@ function draw() {
         text("School Wars", 400, 300);
         textSize(30);
         text("Press Enter to Start", 400, 400);
+        
     }
 
     if(mode == 1){
-        fill(26, 53, 232);
-        rect(100, 100, 100, 100);
-        fill(232, 30, 57);
-        circle(500, 150, 100, 100);
-        
-
+       drawShapes();
+       countDown();
         
     }
 }
@@ -58,4 +59,35 @@ function keyPressed(){
     if(keyCode == ENTER){
         mode = 1;
     }
+}
+
+function drawShapes(){
+    fill(26, 53, 232);
+    rect(100, 100, 100, 100);
+    fill(232, 30, 57);
+    circle(500, 150, 100, 100);
+    console.log(mouseX, mouseY);
+    fill(61, 226, 35);
+    triangle(145, 377, 99, 480, 190, 480);
+    fill(255, 255, 4);
+    quad(476, 420, 526, 360, 526, 471, 500, 420);
+}
+
+function countDown(){
+    timer -= 1 / speedUp;
+    if (timer < 0) {
+        timer = 25;
+        
+        if(speedUp != 20){
+            speedUp -= 20;
+        }
+        else{
+            speedUp = 20;
+        }
+    }
+    console.log(speedUp);
+    textFont(chalkFont);
+    textSize(60);
+    fill(255);
+    text(round(timer), 359, 651);
 }
