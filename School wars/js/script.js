2793bda88c1884568ae3aba378be55d654cf4eb6
casafresca <54688129+casafresca@users.noreply.github.com>
2023-04-13 00:32:39 -0400

    finished game

--- School wars/js/script.js
+++ School wars/js/script.js
@@ -22,7 +22,9 @@ var chalkFont;
 var joystixFont;
 var luckiestGuyFont;
 var RussoOneFont;
-var VT323;
+var VT323Font;
+var rexliaFont;
+var backTo1982Font;
 
 var score = 0;
 var combo = 0;
@@ -33,18 +35,21 @@ var item2 = 0;
 var item3 = 0;
 var item4 = 0;
 var randBackgroundNum = 0;
+var textBox;
+
 var answered = false;
 var firstChoice = true;
 var isPressed = true;
 
-let currentTime = Date.now();
 var bg;
-
-const TIME_BETWEEN_RANDOMIZATIONS = 5000; // milliseconds between new randoms
-
 var backgrounds = [];
 var groceryStoreBg;
 var postOfficeBg;
+var libraryBg;
+var classRoomBg;
+var titleBg;
+var purpleBg;
+var textBoxBg;
 
 var groceryItems = [];
 var apple;
@@ -58,34 +63,91 @@ var box;
 var stamp;
 var tape;
 
+var classRoomItems = [];
+var backpack;
+var chalk;
+var globe;
+var laptop;
+
+var libraryItems = [];
+var bag;
+var beanBag;
+var book;
+var lCard;
+
+var titleMusic; 
+var gameMusic;
+var gameOverMusic;
+
+var dummyNum = 0;
+var dummyNum2 = 0;
+var dummyNum3 = 0;
 
 /**
 Description of preload
 */
 function preload() {
+    //Fonts
     chalkFont = loadFont('assets/fonts/Chalktastic-r78L.ttf');
+    joystixFont = loadFont('assets/fonts/joystix monospace.otf');
+    luckiestGuyFont = loadFont('assets/fonts/LuckiestGuy-Regular.ttf');
+    rexliaFont = loadFont('assets/fonts/rexlia rg.otf');
+    RussoOneFont = loadFont('assets/fonts/RussoOne-Regular.ttf');
+    VT323Font = loadFont('assets/fonts/VT323-Regular.ttf');
+    backTo1982Font = loadFont('assets/fonts/BACKTO1982.TTF');
+
+    //Music
+    titleMusic = loadSound('assets/sounds/Peachy_-_half.cool.mp3');
+    gameMusic = loadSound('assets/sounds/game_music.mp3');
+    gameOverMusic = loadSound('assets/sounds/game-over-38511.mp3');
 
     //backgrounds
-    groceryStoreBg = loadImage('assets/images/backgrounds/grocery_store.jpg');
+    groceryStoreBg = loadImage('assets/images/backgrounds/grocery_store.jpg'); 
     postOfficeBg = loadImage('assets/images/backgrounds/post_office.jpg');
+    libraryBg = loadImage('assets/images/backgrounds/Library.jpg');
+    classRoomBg = loadImage('assets/images/backgrounds/Class_Room.jpg');
+    backgrounds = [groceryStoreBg, postOfficeBg, libraryBg, classRoomBg];
 
-    backgrounds = [groceryStoreBg, postOfficeBg];
+    //title/gameOver background
+    titleBg = loadImage('assets/images/backgrounds/Title.png');
 
+    //story backgrounds
+    purpleBg = loadImage('assets/images/backgrounds/purplebg.png');
+    textBoxBg = loadImage('assets/images/backgrounds/Textwithbg.png');
+    textBox = loadImage('assets/images/backgrounds/Text_bubbles.png');
+
+    /*Items*/
     //grocery store 
-    apple = loadImage('assets/images/grocery_store/Apple.png');
+    apple = loadImage('assets/images/grocery_store/Apple.png'); 
     basket = loadImage('assets/images/grocery_store/Basket.png');
-    croissant = loadImage('assets/images/grocery_store/Croissant.png');
+    croissant = loadImage('assets/images/grocery_store/croissant.png');
     knife = loadImage('assets/images/grocery_store/Knife.png');
 
     groceryItems = [apple, basket, croissant, knife];
 
     //post office
-    letter = loadImage('assets/images/post_office/Letter.png');
-    box = loadImage('assets/images/post_office/Package.png');
-    stamp = loadImage('assets/images/post_office/Stamp.png');
-    tape = loadImage('assets/images/post_office/Tape.png');
+    letter = loadImage('assets/images/post_office/letter.png');
+    box = loadImage('assets/images/post_office/box.png');
+    stamp = loadImage('assets/images/post_office/stamp.png');
+    tape = loadImage('assets/images/post_office/tape.png');
 
-    postOfficeItems = [letter, box, stamp, tape];
+    postOfficeItems = [letter, tape, box, stamp];
+
+    //class room
+    backpack = loadImage('assets/images/class_room/Backpack.png');
+    chalk = loadImage('assets/images/class_room/Chalk.png');
+    globe = loadImage('assets/images/class_room/Globe.png');
+    laptop = loadImage('assets/images/class_room/Laptop.png');
+
+    classRoomItems = [laptop, chalk, backpack, globe];
+    
+    //library
+    bag = loadImage('assets/images/library/Bag.png');
+    beanBag = loadImage('assets/images/library/Beanbag.png');
+    book = loadImage('assets/images/library/Book.png');
+    lCard = loadImage('assets/images/library/Library_card.png');
+
+    libraryItems = [book, bag, beanBag, lCard];
 
 }
 
@@ -93,11 +155,11 @@ function preload() {
 Description of setup
 */
 function setup() {
-gameScreen = createCanvas(windowWidth, windowHeight);
-gameScreen.style('display', 'block');
-frameRate(60);
-background(50);
-imageMode(CENTER);
+    gameScreen = createCanvas(windowWidth, windowHeight);
+    gameScreen.style('display', 'block');
+    frameRate(60);
+    background(50);
+    imageMode(CENTER);
 
 }
 
@@ -106,72 +168,115 @@ Description of draw()
 */
 function draw() {
     clear();
-    background(150, 150, 100);
-    if(mode == 0){
-        textAlign(CENTER);
-        textSize(60);
-        text("School Wars", width/2, height/2);
-        textSize(30);
-        text("Press Enter to Start", width/2, height/2 + 90);
+    image(titleBg, width/2, height/2, width, height);
+    if (mode == 0) {
         
+        if(dummyNum == 0){
+            titleMusic.play();
+            dummyNum++;
+        }
+        
+        textFont(backTo1982Font);
+        textAlign(CENTER);
+        textSize(90);
+        fill(129, 253, 224);
+        text("Pattern Smash", width / 2, height / 2);
+        textFont(rexliaFont);
+        textSize(50);
+        fill(0);
+        text("Press Enter to Start", width / 2, height / 2 + 90);
+
     }
 
     if(mode == 1){
-        startTimer();
-        timeIt();
-        setTimeout(startGame, 2500);
-        
+        image(purpleBg, width/2, height/2, width, height);
+        image(textBox, width/2, height/2, 1280, 1020);
+        text("Please enter space to continue", width/2, height - height/10);
     }
 
-    if(mode == 2){
+    if (mode == 2) {
+        startTimer();
+        timeIt();
+        setTimeout(startGame, 2500); //this is in draw meaning that youre setting a call of startGame EVERY FRAME THAT MODE == 1, i.e startGame will be ran once for every frame that mode == 1
+
+    }
+
+    if (mode == 3) {
+        
+        if(dummyNum2 == 0){
+            titleMusic.stop();
+            gameMusic.play();
+            dummyNum2++;
+        }
+        
         drawShapes();
         countDown();
         drawLives();
         drawCombo();
         drawRound();
         points();
-        
+
     }
 
-    if(mode == 3){
+    if (mode == 4) {
+        
+        if(dummyNum3 == 0){
+            gameMusic.stop();
+            gameOverMusic.play();
+            dummyNum3++;
+        }
         textAlign(CENTER);
         textSize(60);
-        text("Game Over", width/2, height/2);
+        fill(233, 14, 36);
+        text("Game Over", width / 2, height / 2);
     }
 }
 
-function keyPressed(){
-    if(keyCode == ENTER && mode == 0){
+function keyPressed() {
+    if (keyCode == ENTER && mode == 0) {
         mode = 1;
+       
     }
 
-    if (mode == 1) {
+    if( mode == 1){
+        bg = backgrounds[floor(random(4))];
+        nextPress(); //generates your random sequence, once, for your first round. -t
+        if(keyCode == 32){
+            mode = 2;
+        }
+    }   
+
+    if (mode == 2) {
         console.log('pressed' + keyCode);
         console.log("correct" + arrShape[item]);
-        
+
+    }
+
+    if (mode == 3) { //when in the game, hitting a key runs keyFunction -t
+        keyFunction(keyCode);
     }
 }
 
-function drawShapes(){
-    bg = backgrounds[0];
+function drawShapes() {
+   
     if (bg == backgrounds[0]) {
         image(backgrounds[0], width / 2, height / 2, width, height);
 
         for (var i = 0; i < 4; i++) {
             if (i == 0) {
-                image(groceryItems[item], width / 8, height / 2, 200, 200);
+                image(groceryItems[item], width / 5, height / 2, 250, 250);
             }
 
             if (i == 1) {
-                image(groceryItems[item2], width / 2 - 200, height / 2, 200, 200);
+                image(groceryItems[item2], width / 2 - 250, height / 2, 250, 250);
             }
 
             if (i == 2) {
-                image(groceryItems[item3], width / 2 + 200, height / 2, 200, 200);
+                image(groceryItems[item3], width / 2 + 250, height / 2, 250, 250);
             }
 
             if (i == 3) {
-                image(groceryItems[item4], width - width / 8, height / 2, 200, 200);
+                image(groceryItems[item4], width - width / 5, height / 2, 250, 250);
             }
         }
     }
@@ -179,69 +284,109 @@ function drawShapes(){
         image(backgrounds[1], width / 2, height / 2, width, height);
         for (var i = 0; i < 4; i++) {
             if (i == 0) {
-                image(postOfficeItems[item], width / 8, height / 2, 200, 200);
+                image(postOfficeItems[item], width / 5, height / 2, 250, 250);
             }
 
             if (i == 1) {
-                image(postOfficeItems[item2], width / 2 - 200, height / 2, 200, 200);
+                image(postOfficeItems[item2], width / 2 - 250, height / 2, 250, 250);
             }
 
             if (i == 2) {
-                image(postOfficeItems[item3], width / 2 + 200, height / 2, 200, 200);
+                image(postOfficeItems[item3], width / 2 + 250, height / 2, 250, 250);
             }
 
             if (i == 3) {
-                image(postOfficeItems[item4], width - width / 8, height / 2, 200, 200);
+                image(postOfficeItems[item4], width - width / 5, height / 2, 250, 250);
             }
         }
     }
-    
+    else if(bg == backgrounds[2]){
+        image(backgrounds[2], width / 2, height / 2, width, height);
+        for (var i = 0; i < 4; i++) {
+            if (i == 0) {
+                image(libraryItems[item], width / 5, height / 2, 250, 250);
+            }
+
+            if (i == 1) {
+                image(libraryItems[item2], width / 2 - 250, height / 2, 250, 250);
+            }
+
+            if (i == 2) {
+                image(libraryItems[item3], width / 2 + 250, height / 2, 250, 250);
+            }
+
+            if (i == 3) {
+                image(libraryItems[item4], width - width / 5, height / 2, 250, 250);
+            }
+        }
+    }
+    else if(bg == backgrounds[3]){
+        image(backgrounds[3], width / 2, height / 2, width, height);
+        for (var i = 0; i < 4; i++) {
+            if (i == 0) {
+                image(classRoomItems[item], width / 5, height / 2, 250, 250);
+            }
+
+            if (i == 1) {
+                image(classRoomItems[item2], width / 2 - 250, height / 2, 250, 250);
+            }
+
+            if (i == 2) {
+                image(classRoomItems[item3], width / 2 + 250, height / 2, 250, 250);
+            }
+
+            if (i == 3) {
+                image(classRoomItems[item4], width - width / 5, height / 2, 250, 250);
+            }
+        }
+    }
+
 }
 
-function countDown(){
+function countDown() {
 
     timer -= 1 / speedUp;
     if (timer <= 0) {
         loselives();
         timer = 25;
-        
-    } 
+
+    }
     //console.log(speedUp);
-    textFont(chalkFont);
+    textFont(rexliaFont);
     textSize(60);
     fill(255);
-    text(floor(timer), width/2, height-height/10);
-    
-    
+    text(floor(timer), width / 2, height - height / 10);
+
+
 }
 
 function drawLives() {
-    textFont(chalkFont);
+    textFont(rexliaFont);
     textSize(60);
     fill(255);
-    text("Lives " + lives, width-width/8, height-height/10);
-    
+    text("Lives " + lives, width - width / 8, height - height / 10);
+
 }
 
 function drawCombo() {
-    textFont(chalkFont);
+    textFont(rexliaFont);
     textSize(60);
     fill(255);
-    text("Combo " + combo,  width-width/8, height/10);   
-    
+    text("Combo " + combo, width - width / 8, height / 10);
+
 }
 
-function drawRound(){
-    textFont(chalkFont);
+function drawRound() {
+    textFont(rexliaFont);
     textSize(60);
     fill(255);
-    text("Round " + roundNum, width/2, height/10);
+    text("Round " + roundNum, width / 2, height / 10);
 }
 
-function points(){
-    fill(255, 255, 4);
-    text("Score = " + score, width/8, height/10);
-    
+function points() {
+    fill(255);
+    text("Score = " + score, width / 8, height / 10);
+
 }
 
 function getPoint() {
@@ -252,8 +397,9 @@ function getPoint() {
     nextPress();
     if (roundCount % 4 == 0) {
         roundNum++;
+        bg = backgrounds[floor(random(4))];
         nextRound();
-        //bg = backgrounds[floor(random(2))];
+        
 
     }
 }
@@ -263,7 +409,7 @@ function loselives() {
     combo = 0;
     lives -= 1;
     if (lives < 1) {
-        mode = 3;
+        mode = 4;
     }
 }
 
@@ -276,49 +422,54 @@ function nextRound() {
         speedUp = 20;
     }
     console.log("round change");
-    randBackgroundNum = floor(random(2));
+    randBackgroundNum = floor(random(4));
 }
 
 function nextPress() {
+    itemList.splice(0, itemList.length); //clears the itemList array -t
     item = floor(random(4));
     item2 = floor(random(4));
     item3 = floor(random(4));
     item4 = floor(random(4));
-    currentTime = Date.now();
+    itemList = [item, item2, item3, item4]; //sets up your item list with the randomly generated items -t
+    itemIndex = 0; //reset itemIndex to determine in which order you need to hit the items -t
 }
 
 
 function startTimer() {
     if (startTime > 1) {
-        text(Math.round(startTime), width/2, height/2);
-        
+        text(Math.round(startTime), width / 2, height / 2);
+
     } else {
         text("GO", width / 2, height / 2);
     };
-    
+
 }
 
 function timeIt() {
-    if(startTime > 0){
+    if (startTime > 0) {
         startTime -= deltaTime / 1000;
     }
 
 }
 
-function startGame(){
-    mode = 2;
+function startGame() {
+    mode = 3;
 }
 
-
-function keyFunction(){
-    if(bg = backgrounds[0]){
-        if (keyIsDown(UP_ARROW) && groceryItems[item] == 0 
-        || keyIsDown(UP_ARROW) && groceryItems[item2] == 0 
-        || keyIsDown(UP_ARROW) && groceryItems[item3] == 0 
-        || keyIsDown(UP_ARROW) && groceryItems[item4] == 0){
+var itemIndex = 0; //which item in the list are we on? -t
+var itemList = []; //contains item 1 through 4 in a handy dandy array -t
+function keyFunction(KEYCODE) { //called on keyPressed when mode == 2 -t
+    print("received keyFunction" + keyCode);
+    if (KEYCODE == arrShape[itemList[itemIndex]]) { //basically we are using arrShape which stores the keyCodes from WASD in order 0 through 3 and comparing it with the input keyCode, to determine WHICH value in arrShape we use itemList which contains our four items in order, and we use itemIndex to keep track of which item we are currently on. -t
+        print("Correct!");
+        //  getPoint();
+        itemIndex++;
+        if (itemIndex >= itemList.length) {
             getPoint();
         }
+    } else {
+        print("wrong");
+        loselives();
     }
 } 
-
-
